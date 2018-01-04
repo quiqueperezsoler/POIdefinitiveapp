d2e1ca4e6747bfae9dd6752577345b60e18267ce
Enrique Perez Soler <quiqueperezsoler@gmail.com>
2018-01-04 12:11:48 +0100

    Added buttons to map screen

--- app/src/main/java/com/example/quique/poi/MapsActivity.java
+++ app/src/main/java/com/example/quique/poi/MapsActivity.java
@@ -39,8 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("You are here!"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setBuildingsEnabled(true);
     }
 }
